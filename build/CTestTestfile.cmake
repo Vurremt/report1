c9c8d4c1c8c753f524dbaeec2f8139f18e1a69b6
Vurremt <72647204+Vurremt@users.noreply.github.com>
2024-05-12 22:09:52 +0900

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/evahn/Desktop/Cours Kyushu/Computer Vision/report1
-# Build directory: C:/Users/evahn/Desktop/Cours Kyushu/Computer Vision/report1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
